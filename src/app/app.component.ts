1c392b530d43c2919f8cac1b3ee5796cf9409563
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-12-29 11:07:51 +0900

    [add] added member class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Member {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{member}}</h2>
-  `;
-
+    <h2>{{member.name}}</h2>
+  `,
 })
 export class AppComponent{
-  title = "社員管理名簿"
-  member = "山田太郎"
+  title = "社員管理名簿";
+  member: Member = {
+    id: 1,
+    name: "山田太郎"
+  }
 }
