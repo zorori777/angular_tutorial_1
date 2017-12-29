276a948bd446de1f5e59535cf69a150321fc8836
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-12-29 10:58:52 +0900

    [add] added title and member

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{member}}</h2>
+  `;
+
 })
 export class AppComponent{
+  title = "社員管理名簿"
+  member = "山田太郎"
 }
