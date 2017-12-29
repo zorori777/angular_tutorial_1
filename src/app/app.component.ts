7f2ed077123b3d5af977bc48246731f031365f7c
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-12-29 11:12:16 +0900

    [add] added label of id and name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,11 @@ export class Member {
   template: `
     <h1>{{title}}</h1>
     <h2>{{member.name}}</h2>
-  `,
+    <div><label>id:</label> {{member.id}}</div>
+    <div><label>name:</label> {{member.name}}</div>
+    `,
 })
+
 export class AppComponent{
   title = "社員管理名簿";
   member: Member = {
